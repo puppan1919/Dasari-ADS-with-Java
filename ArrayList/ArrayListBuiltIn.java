d911f32aeb1d0a51d42627ea9cfac7dd7174f1b6
Parne manohar reddy <parnemanoharreddy19@gmail.com>
2024-04-22 15:10:51 +0530

    Update ArrayListBuiltIn.java

--- ArrayList/ArrayListBuiltIn.java
+++ ArrayList/ArrayListBuiltIn.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class ArrayListBuiltIn {
     Scanner s1 = new Scanner(System.in);
     public static void main(String[] args){
-        
+        System.out.println("Enter the value of the Element: ");
+        int Element = s1.nextInt();
     }
 }
